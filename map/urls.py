4ca6bb9192b029fba38adc1732dc17ab3416757e
adityadhamanekar <adityadhamanekar725@gmail.com>
2024-12-10 09:53:37 +0530

    some features added

--- map/urls.py
+++ map/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.map_view, name='map_view'),  # Add a path for the map
+    path('', views.map_view, name='map'),  # Add a path for the map
 ]
